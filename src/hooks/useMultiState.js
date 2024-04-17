8ae8ec055717c30e4956ac7782e34d8dfa235379
begu0025 <emilia.beguin@etudiant.univ-reims.fr>
2024-04-17 16:55:37 +0200

    création du hook pour stocker plusieurs champs dans un seul equivalent useState

---
+++ src/hooks/useMultiState.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+export default function useMultiState() {
+    const [data, setData] = useState({});
+
+    function addState(key, value) {
+        if (value === "") {
+            deleteState(key);
+        } else {
+            setData({...data, [key]: value});
+        }
+    }
+
+    function getState(key) {
+        return data[key];
+    }
+
+    function deleteState(key) {
+        delete data[key];
+        setData({...data});
+    }
+
+    return [data, addState, getState, deleteState];
+}
