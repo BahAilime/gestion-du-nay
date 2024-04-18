c8b6140db35f5bd50d51fb52d79be3cbad453a44
begu0025 <emilia.beguin@etudiant.univ-reims.fr>
2024-04-18 10:38:02 +0200

    Ajout du keyfilter pour les FormInputs

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { InputText } from 'primereact/inputtext';
 
-export default function FormInput({ label, name, value = "", onChange = (x) => {}}) {
+export default function FormInput({ label, name, value = "", onChange = (x) => {}, keyfilter = "" }) {
     return (
-        <div className="flex flex-col gap-1 text-nay-white-100 my-4">
+        <div className="flex flex-col gap-1 mt-2 mb-3">
             <label htmlFor={name} value={value}>{label}</label>
-            <InputText id={name} aria-describedby={`${name}-help`} onChange={(e) => onChange(e.target.value)} />
+            <InputText id={name} aria-describedby={`${name}-help`} onChange={(e) => onChange(e.target.value)} keyfilter={keyfilter}/>
         </div>
     );
 }
