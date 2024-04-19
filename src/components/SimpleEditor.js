5078b2db2cc3dac07629878fa911d10060ae7cff
begu0025 <emilia.beguin@etudiant.univ-reims.fr>
2024-04-19 09:36:38 +0200

    Fix valeur par defaut + class en className

--- src/components/SimpleEditor.js
+++ src/components/SimpleEditor.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { Editor } from 'primereact/editor';
 
-export default function FormInput({ label, value = "", onChange = (x) => {} }) {
+export default function FormInput({ label = "", value = "", onChange = (x) => {} }) {
     const [text, setText] = useState('<div>Hello World!</div><div>PrimeReact <b>Editor</b> Rocks</div><div><br></div>');
 
     const renderHeader = () => {
         return (
             <>
-                <select class="ql-size">
+                <select className="ql-size">
                     <option value="huge">Enorme</option>
                     <option value="large">Grand</option>
                     <option selected>Normal</option>
@@ -26,7 +26,7 @@ export default function FormInput({ label, value = "", onChange = (x) => {} }) {
 
     return (
         <div className="my-2">
-            <label value={value}>{label}</label>
+            {label && <label value={value}>{label}</label> }
             <Editor
                 value={text}
                 onTextChange={(e) => {
