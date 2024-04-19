7f20e54f32b72703da67cb33e37ed0a1ef5d120e
begu0025 <emilia.beguin@etudiant.univ-reims.fr>
2024-04-19 08:46:24 +0200

    Ajout de ma 1ère migration (client)

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,31 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use tauri_plugin_sql::{Migration, MigrationKind};
 
 fn main() {
+  let migrations = vec![
+    Migration {
+        version: 1,
+        description: "Table des clients",
+        sql: "CREATE TABLE client (
+          id_cli INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+          nom_cli INTEGER,
+          resp_cli TEXT,
+          email_cli TEXT,
+          tel_cli TEXT,
+          adr_cli TEXT,
+          cp_cli INTEGER,
+          ville_cli TEXT
+        );",
+        kind: MigrationKind::Up,
+    }
+  ];
+
   tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+      .plugin(
+        tauri_plugin_sql::Builder::default()
+          .add_migrations("sqlite:nay.db", migrations)
+          .build())
+      .run(tauri::generate_context!())
+      .expect("error while running tauri application");
 }
