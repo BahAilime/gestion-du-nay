db82b2f6e6ee605d13974bf960a469546ed211f3
begu0025 <emilia.beguin@etudiant.univ-reims.fr>
2024-04-19 11:17:18 +0200

    Fix value sur le mauvais élément

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -4,8 +4,8 @@ import { InputText } from 'primereact/inputtext';
 export default function FormInput({ label, name, value = "", onChange = (x) => {}, keyfilter = "" }) {
     return (
         <div className="flex flex-col gap-1 mt-2 mb-3 w-full">
-            <label htmlFor={name} value={value}>{label}</label>
-            <InputText id={name} aria-describedby={`${name}-help`} onChange={(e) => onChange(e.target.value)} keyfilter={keyfilter}/>
+            <label htmlFor={name}>{label}</label>
+            <InputText value={value} id={name} aria-describedby={`${name}-help`} onChange={(e) => onChange(e.target.value)} keyfilter={keyfilter}/>
         </div>
     );
 }
