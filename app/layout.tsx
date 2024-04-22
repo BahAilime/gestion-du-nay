949862f5884bbb02deade7903d31bb19fd082cac
begu0025 <emilia.beguin@etudiant.univ-reims.fr>
2024-04-22 08:38:48 +0200

    Ajout des nouveaux components dans le layout (menu, nay et breadcrumb)

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,30 +8,23 @@ import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false;
 
 // NextJs
+import Image from "next/image";
 import Link from "next/link";
-import { Inter } from "next/font/google";
-import { usePathname } from 'next/navigation';
+import { Gabarito } from "next/font/google";
 
 // PrimeReact
 import { PrimeReactProvider } from 'primereact/api';
-import { BreadCrumb } from 'primereact/breadcrumb';
+import BreadCrumb from '../src/components/BreadCrumb';
+import Menu from '../src/components/Menu';
+import Nay from '../src/components/NaySvg';
 
-const inter = Inter({ subsets: ["latin"] });
+const gabarito = Gabarito({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-  const pathlist = usePathname().split('/')
-  const breadcrumbItems = []
-  for (let i = 0; i < pathlist.length; i++) {
-    if (pathlist[i] === "") continue
-    breadcrumbItems.push({
-      template: () => <Link href={"/" + pathlist.slice(1, i + 1).join("/")}>{pathlist[i][0].toUpperCase() + pathlist[i].slice(1)}</Link>
-    })
-  }
 
   return (
     <PrimeReactProvider>
@@ -48,36 +41,16 @@ export default function RootLayout({
                 }}
               />
       </head>
-        <body className={`h-screen w-full flex flex-row ${inter.className}`}>
-          <aside className="h-full w-64 flex flex-col p-8 content-center gap-8 bg-nay-white">
-            <h1><Link href="/">Hameau du Nay</Link></h1>
-            <div className="">
-              <h2><Link href="/">Accueil</Link></h2>
-              <h2><Link href="/quote">Devis</Link></h2>
-              <ul className="pl-3">
-                <li><Link href="/quote/new">Nouveau devis</Link></li>
-                <li><Link href="/quote/new">Devis en cours</Link></li>
-                <li><Link href="/quote/new">Devis perdus</Link></li>
-              </ul>
-              <h2>Tarifs</h2>
-              <ul className="pl-3">
-                <li>Tarif Généraux</li>
-                <li>Accords</li>
-                <li>
-                  <ul className="pl-3">
-                    <li>Nouvel accord</li>
-                    <li>Accord 1</li>
-                    <li>Accord 2</li>
-                    <li>Accord 3</li>
-                    <li>Accord 4</li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
+        <body className={`h-screen w-full flex flex-row ${gabarito.className}`}>
+          <aside className="h-full flex flex-col p-8 content-center gap-3 bg-nay-white">
+            <Nay
+              className='fill-nay-cyan-300 w-16 mx-auto'
+            />
+            <Menu />
           </aside>
           <main className="h-screen w-full flex flex-col">
             <header className="m-5">
-              <h1><BreadCrumb model={breadcrumbItems} home={{ label: 'Hameau du Nay', url: '/' }} /></h1>
+              <BreadCrumb />
             </header>
             <main className="h-full w-full bg-gray-200 p-4 overflow-y-scroll">
               {children}
