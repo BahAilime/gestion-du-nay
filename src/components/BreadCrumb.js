2343ba97e1a8c1f414c15ac073ee8649160a075d
begu0025 <emilia.beguin@etudiant.univ-reims.fr>
2024-04-22 08:37:17 +0200

    Déplacement de breadcrub dans son propre component

---
+++ src/components/BreadCrumb.js
@@ -0,0 +1,22 @@
+import { usePathname } from 'next/navigation';
+import Link from "next/link";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+import { BreadCrumb as Bread } from 'primereact/breadcrumb';
+
+export default function BreadCrumb() {
+    const pathlist = usePathname().split('/')
+    const breadcrumbItems = []
+    for (let i = 0; i < pathlist.length; i++) {
+        if (pathlist[i] === "") continue
+        breadcrumbItems.push({
+            template: () => <Link href={"/" + pathlist.slice(1, i + 1).join("/")}>{pathlist[i][0].toUpperCase() + pathlist[i].slice(1)}</Link>
+        })
+    }
+
+    const home = { template: () => <Link href="/"><FontAwesomeIcon icon={faHouse} className='text-nay-orange' /></Link>}
+
+    return (
+        <Bread model={breadcrumbItems} home={home} className='w-max'/>
+    )
+}
