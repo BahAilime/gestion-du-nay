784a4e906e2a8da2efe6e5eaa9343b8a2fe386fb
begu0025 <emilia.beguin@etudiant.univ-reims.fr>
2024-04-24 16:35:42 +0200

    Ajout de la page d'enregistrement de nouveau client

---
+++ app/client/nouveau/page.tsx
@@ -0,0 +1,41 @@
+'use client'
+import { useState } from 'react';
+import ClientForm from "../../../src/components/ClientForm";
+import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquareCheck } from '@fortawesome/free-solid-svg-icons'
+import { ref, push } from "firebase/database"
+import { db } from "@/src/services/firebase";
+import { useRouter } from 'next/navigation'
+
+
+export default function Home() {
+    const [data, setData] = useState({});
+    const router = useRouter()
+    const searchParams = new URLSearchParams();
+    
+    function save() {
+      const clientRef = ref(db, "client")
+      push(clientRef, data)
+          .then((oh) => {
+            try {
+              searchParams.append("firebase_id", oh.key ?? "error")
+              router.push(`/client/view?${searchParams.toString()}`)
+            } catch (error) {
+              console.log(error)
+            }
+          })
+    }
+
+
+    return (
+        <Card title="Client" className="min-w-1/2">
+          <ClientForm onChange={(x) => setData(x)} dataImport={{nom_cli: "Euuhhh"}}/>
+          <Button onClick={save} className="w-full flex justify-center gap-2">
+            <FontAwesomeIcon icon={faSquareCheck} />
+            Enregistrer le client
+          </Button>
+        </Card>
+    );
+  }
