7eca4bfef7dc6e422ca71fea28e5b31cfe9655bd
BahAilime <emilia.beguin@gmail.com>
2024-04-30 14:56:50 +0200

    Loading des clients

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -1,17 +1,15 @@
 'use client'
 import { useEffect, useState } from 'react';
 import { Card } from 'primereact/card';
-import { Button } from 'primereact/button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSquareCheck } from '@fortawesome/free-solid-svg-icons'
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { ref, onValue } from "firebase/database"
 import { db } from "@/src/services/firebase";
 import { useRouter } from 'next/navigation'
+import Loading from '@/src/components/Loading';
 
 export default function Home() {
-    const [client, setClient] = useState<any[]>([]);
+    const [client, setClient] = useState<any[]>();
     const clientRef = ref(db, 'client');
     const router = useRouter();
 
@@ -32,12 +30,14 @@ export default function Home() {
 
     return (
         <Card title="Clients" className="w-full h-full">
-            <DataTable value={client} className="w-full" selectionMode="single" onSelectionChange={(e) => router.push(`/client/view?firebase_id=${e.value.firebase_id}`)}>
+            {client ?
+            <DataTable  value={client} className="w-full" selectionMode="single" onSelectionChange={(e) => router.push(`/client/view?firebase_id=${e.value.firebase_id}`)}>
                 <Column field="nom_cli" header="Nom"></Column>
                 <Column field="resp_cli" header="Resp"></Column>
                 <Column field="email_cli" header="Email"></Column>
-                <Column field="tele_cli" header="Tel"></Column>
+                <Column field="tel_cli" header="Tel"></Column>
             </DataTable>
+            : <Loading text={"Chargement des clients..."}/>}
         </Card>
     );
   }
