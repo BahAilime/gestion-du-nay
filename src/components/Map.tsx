c478b2aed7b574a98c0f575c9a8738cbb602d442
BahAilime <emilia.beguin@gmail.com>
2024-05-02 11:55:00 +0200

    Fix map qui ne se mettais pas à jour lorsque la position change

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -8,7 +8,12 @@ import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome } from '@fortawesome/free-solid-svg-icons'
 
-export default function Map({ position, zoom, icon, homePosition = [46.878946414834765, -0.6522204475188746] }: { position: [number, number], zoom: number, icon?: any, homePosition?: [number, number] }) {
+export default function Map({ position, zoom, icon, homePosition = [46.878946414834765, -0.6522204475188746], height="400px" }: { position: [number, number], zoom: number, icon?: any, homePosition?: [number, number], height?: string }) {
+  if (!Array.isArray(position) || position.length !== 2 || typeof position[0] !== 'number' || typeof position[1] !== 'number') {
+    console.warn("Position doit être un array de deux nombres. Position:", position)
+    return (<div> Erreur position: {JSON.stringify(position)}</div>)
+  }
+  
   const customMarkerIcon = icon ? new Leaflet.DivIcon({
     html: ReactDOMServer.renderToString(<FontAwesomeIcon icon={icon} />),
     iconSize: [32, 32],
@@ -22,17 +27,19 @@ export default function Map({ position, zoom, icon, homePosition = [46.878946414
   });
 
   return (
-    <MapContainer center={position} zoom={zoom} scrollWheelZoom={false} className="h-full">
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      { customMarkerIcon && <Marker position={position} icon={customMarkerIcon} /> }
-      <Marker position={homePosition} icon={nayMarkerIcon}>
-        <Popup>
-          Le Hameau du Nay<br />8 Le Nay, 79140 Le Pin
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div style={{ height: height }}>
+      <MapContainer center={position} zoom={zoom} scrollWheelZoom={false} className="h-full" key={position[0].toString()}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        { customMarkerIcon && <Marker position={position} icon={customMarkerIcon} /> }
+        <Marker position={homePosition} icon={nayMarkerIcon}>
+          <Popup>
+            Le Hameau du Nay<br />8 Le Nay, 79140 Le Pin
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   )
 }
