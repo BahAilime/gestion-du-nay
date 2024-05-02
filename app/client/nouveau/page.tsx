2b2a7ad888d6449c08cdcd53d5c3b9edb8c6e826
BahAilime <emilia.beguin@gmail.com>
2024-05-02 14:09:36 +0200

    Refactor page nouveau client

--- app/client/nouveau/page.tsx
+++ app/client/nouveau/page.tsx
@@ -2,11 +2,9 @@
 import { useState } from 'react';
 import ClientForm from "../../../src/components/ClientForm";
 import { Card } from 'primereact/card';
-import { Button } from 'primereact/button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquareCheck } from '@fortawesome/free-solid-svg-icons'
-import { ref, push } from "firebase/database"
-import { db } from "@/src/services/firebase";
+import BigButton from "@/src/components/bigButton";
+import { newClient } from "@/src/services/db";
 import { useRouter } from 'next/navigation'
 
 
@@ -16,26 +14,20 @@ export default function Home() {
     const searchParams = new URLSearchParams();
     
     function save() {
-      const clientRef = ref(db, "client")
-      push(clientRef, data)
-          .then((oh) => {
-            try {
-              searchParams.append("firebase_id", oh.key ?? "error")
+      newClient(data, (client) => {
+        try {
+          searchParams.append("firebase_id", client.key ?? "error")
           router.push(`/client/detail?${searchParams.toString()}`)
-            } catch (error) {
-              console.log(error)
-            }
-          })
+        } catch (error) {
+          console.log(error)
+        }
+      })
     }
-
 
     return (
         <Card title="Client" className="min-w-1/2">
-          <ClientForm onChange={(x) => setData(x)} dataImport={{nom_cli: "Euuhhh"}}/>
-          <Button onClick={save} className="w-full flex justify-center gap-2">
-            <FontAwesomeIcon icon={faSquareCheck} />
-            Enregistrer le client
-          </Button>
+          <ClientForm onChange={(data) => setData(data)}/>
+          <BigButton text="Enregistrer le client" onClick={save} icon={faSquareCheck} />
         </Card>
     );
   }
