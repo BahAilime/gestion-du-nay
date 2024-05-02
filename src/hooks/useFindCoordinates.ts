03942835778a626bd655b1245251e6128e1efde0
BahAilime <emilia.beguin@gmail.com>
2024-05-02 11:50:10 +0200

    Fix useFindCoordinates

--- src/hooks/useFindCoordinates.ts
+++ src/hooks/useFindCoordinates.ts
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-export default function useFindCoordinates(searchInfos = []) {
+export default function useFindCoordinates() {
   const [coordinates, setCoordinates] = useState<[number, number]>([621, 926])
 
   /**
@@ -9,43 +9,36 @@ export default function useFindCoordinates(searchInfos = []) {
    * @param {Array} infos - Un tableau d'informations pour lesquelles récupérer les coordonnées.
    * @return {Array} Un tableau contenant les coordonnées du lieu si trouvées, sinon un tableau vide.
    */
-  async function fetchCoordinates (infos) {
+  async function fetchCoordinates(infos: string[]): Promise<[number, number]> {
     const url = `https://nominatim.openstreetmap.org/search?format=json&q=${infos.join(", ")}`
-    const response = await fetch(encodeURI(url))
-    const data = await response.json()
-
-    return data.length > 0 ? [parseFloat(data[0].lat), parseFloat(data[0].lon)] : []
+    try {
+      const response = await fetch(encodeURI(url))
+      const data = await response.json()
+      return data.length > 0 ? [parseFloat(data[0].lat), parseFloat(data[0].lon)] : [621, 926]
+    } catch (error) {
+      console.log(error)
+    }
+    return [621, 926]
   }
 
-  
   /**
    * Une fonction qui définit les coordonnées en fonction de l'array d'informations fourni.
    *
    * @param {Array} infos - Un array d'informations utilisé pour déterminer les coordonnées.
    * @return {void}
    */
-  function setSearchInfos(infos) {
+  async function setSearchInfos(infos: string[]) {
     while (infos.length > 1) {
-      const coordinates = fetchCoordinates(infos)
-      if (coordinates.length === 0) {
+      const coo = await fetchCoordinates(infos)
+      if (Number(coo.length) === 0) {
         infos.shift()
       } else {
         break
       }
     }
 
-    setCoordinates(fetchCoordinates(infos))
+    setCoordinates(await fetchCoordinates(infos))
   }
 
-
-  /**
-   * Détermine si un élément a été trouvé en vérifiant si les coordonnées correspondent aux coordonnées attendues.
-   *
-   * @return {boolean} Retourne true si l'élément a été trouvé, false sinon.
-   */
-  function haveBeenFound() {
-    return coordinates[0] !== 621 || coordinates[1] !== 926
-  }
-
-  return [coordinates, haveBeenFound, setSearchInfos]
+  return [coordinates, setSearchInfos]
 }
