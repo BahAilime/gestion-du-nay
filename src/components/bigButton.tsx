b4b62652513f523126cd645d07d9efbdab8387ed
BahAilime <emilia.beguin@gmail.com>
2024-05-03 09:11:24 +0200

    Ajout du support pour les classes du BigButton

--- src/components/bigButton.tsx
+++ src/components/bigButton.tsx
@@ -1,8 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Button } from 'primereact/button';
 
-export default function BigButton({ text="", onClick=()=>{}, icon = null }: { text?: string, onClick: () => void, icon?: any }) {
-    return (<Button onClick={onClick} className="w-full flex justify-center gap-2">
+export default function BigButton({ text="", onClick=()=>{}, icon = null, className }: { text?: string, onClick: () => void, icon?: any, className?: string }) {
+    return (<Button onClick={onClick} className={"w-full flex justify-center gap-2 " + className}>
         {icon && <FontAwesomeIcon icon={icon} />}
         {text}
     </Button>)
