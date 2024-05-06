2171eedee51f512ded4d5716bd8025022e5defb4
BahAilime <emilia.beguin@gmail.com>
2024-05-06 15:51:55 +0200

    🚀 Config temporaore afin de faire un premier build de l'application -> issue style on reload

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    // TODO: Fix les suspens dans detail / liste clients
+    experimental: {
+      missingSuspenseWithCSRBailout: false,
+    },
   };
 
 export default nextConfig;
