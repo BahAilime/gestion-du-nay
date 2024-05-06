73b7f17be5aebc6f90c2ba9ac2eaaad136c0b079
BahAilime <emilia.beguin@gmail.com>
2024-05-06 08:51:35 +0200

    MenuItem de js a tsx

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 
-export default function MenuItem({ text="", url="#", icon = null }) {
+export default function MenuItem({ text="", url="#", icon = null }: { text?: string, url?: string, icon?: JSX.Element|null }) {
     return (
             <Link className="flex items-center gap-2 w-max ml-1" href={url}>{icon && icon}<h1>{text}</h1></Link>
     )
