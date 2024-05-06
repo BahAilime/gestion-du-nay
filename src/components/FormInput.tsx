e1119bc5df3af90265215fd24ea246c615f58e68
BahAilime <emilia.beguin@gmail.com>
2024-05-06 10:12:58 +0200

    Fix type FormInput

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,10 +1,11 @@
 import { InputText } from 'primereact/inputtext';
+import { KeyFilterType } from 'primereact/keyfilter';
 
-export default function FormInput({ label, name, value = "", onChange = (x) => {}, keyfilter = "" } : { label: string, name: string, value?: string, onChange?: (x: string) => void, keyfilter?: string }) {
+export default function FormInput({ label, name, value = "", onChange = (x) => {}, keyfilter = null } : { label: string, name: string, value?: string, onChange?: (x: string) => void, keyfilter?: KeyFilterType|null }) {
     return (
         <div className="flex flex-col gap-1 mt-2 mb-3 w-full">
             <label htmlFor={name}>{label}</label>
-            <InputText value={value} id={name} aria-describedby={`${name}-help`} onChange={(e) => onChange(e.target.value)} keyfilter={keyfilter}/>
+            <InputText value={value} id={name} aria-describedby={`${name}-help`} onChange={(e) => onChange(e.target.value)} {...(keyfilter && { keyfilter })}/>
         </div>
     );
 }
