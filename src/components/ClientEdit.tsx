59f61ffc42bb7a5374bd571e8d7427b4674cdb20
BahAilime <emilia.beguin@gmail.com>
2024-05-07 15:31:06 +0200

    Création du composant ClientEdit pour le meme dans un Suspense dans la page

---
+++ src/components/ClientEdit.tsx
@@ -0,0 +1,44 @@
+'use client'
+import { useEffect, useState } from 'react';
+import ClientForm from "@/src/components/ClientForm";
+import { Card } from 'primereact/card';
+import { faSquareCheck } from '@fortawesome/free-solid-svg-icons'
+import BigButton from "@/src/components/BigButton";
+import { getClientOnce, updateClient } from "@/src/services/db";
+import { redirect, useRouter, useSearchParams } from 'next/navigation'
+
+export default function ClientEdit() {
+    const [data, setData] = useState({});
+    const [client, setClient] = useState();
+    const router = useRouter()
+    const params = useSearchParams()
+    const firebase_id = params.get("firebase_id")
+
+    useEffect(() => {
+      if (!firebase_id) {
+        redirect("/client")
+      }
+      getClientOnce(firebase_id).then((user) => {   
+          setClient(user)
+      }).catch((error) => {
+          console.error(error);
+      });
+  }, [firebase_id])
+    
+    function update() {
+      if (!firebase_id) {
+        redirect("/client")
+      }
+
+      updateClient(firebase_id, data, () => {
+          router.push(`/client/detail?${params.toString()}`)
+        })
+    }
+
+    return (
+        <Card title="Client" className="min-w-1/2">
+          {client && <ClientForm onChange={(data) => setData(data)} dataImport={client}/>}
+          <BigButton text="Modifier le client" onClick={update} icon={faSquareCheck} />
+        </Card>
+    );
+}
