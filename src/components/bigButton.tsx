615cbc3bbd3f97d025f4a1c36681934e534e5dae
BahAilime <emilia.beguin@gmail.com>
2024-05-07 16:26:08 +0200

    Modification du bouton pour qu'il puisse avoir une severité et un contour

--- src/components/bigButton.tsx
+++ src/components/bigButton.tsx
@@ -1,8 +1,22 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Button } from 'primereact/button';
+import { Button, ButtonProps } from 'primereact/button';
 
-export default function BigButton({ text="", onClick=()=>{}, icon = null, className }: { text?: string, onClick: () => void, icon?: any, className?: string }) {
-    return (<Button onClick={onClick} className={"w-full flex justify-center gap-2 " + className}>
+export default function BigButton({
+    text = "",
+    onClick = () => { },
+    icon = null,
+    className,
+    outlined = false,
+    severity
+}: {
+    text?: string,
+    onClick: () => void,
+    icon?: any,
+    className?: string,
+    outlined?: boolean,
+    severity?: "info" | "secondary" | "success" | "warning" | "danger" | "help" | undefined
+}) {
+    return (<Button onClick={onClick} className={"w-full flex justify-center gap-2 " + className} outlined={outlined} {...(severity && { severity })}>
         {icon && <FontAwesomeIcon icon={icon} />}
         {text}
     </Button>)
