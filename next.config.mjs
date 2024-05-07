711d2b585ac2e514f48565d797b4c3c56fbfe852
BahAilime <emilia.beguin@gmail.com>
2024-05-07 15:42:19 +0200

    Suppr option dangereuse config next

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    // TODO: Fix les suspens dans detail / liste clients
-    experimental: {
-      missingSuspenseWithCSRBailout: false,
-    },
     images: {
       unoptimized: true,
     },
