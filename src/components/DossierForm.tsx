155d8a1c11d4d6802ee28184e3720b7d063ed151
BahAilime <emilia.beguin@gmail.com>
2024-05-22 14:55:34 +0200

    Récupération des infos de "reference" / "base"

--- src/components/DossierForm.tsx
+++ src/components/DossierForm.tsx
@@ -21,7 +21,7 @@ export default function DossierForm() {
         { label: 'Déja venu', icon: <FontAwesomeIcon icon={faHeart} className="mr-2" /> },
     ];
 
-    const [dates, setDates] = useState<{debut?: Date, fin?: Date}>({});
+    const [infos, setInfos] = useState<{debut?: Date, fin?: Date, adultes?: number, enfants?: number, nuits?: number}>({});
     const [client, setClient] = useState();
     const [nuit, setNuit] = useState({});
     const [drap, setDrap] = useState(0);
@@ -32,7 +32,7 @@ export default function DossierForm() {
     return (
         <div className="grid 2xl:grid-cols-3 xl:grid-cols-2 grid-flow-dense gap-2">
             {activeIndex === 0 &&
-            <Card title="Client" className="w-full h-full col-span-2 row-span-3">
+            <Card title="Client" className="w-full h-full col-span-2 row-span-2">
                 <TabMenu model={items} activeIndex={activeIndex} onTabChange={(e) => setActiveIndex(e.index)} />
                 <ClientForm onChange={(clientInfos) => setClient(clientInfos)} />
             </Card>}
@@ -45,12 +45,21 @@ export default function DossierForm() {
 
             <Card className="w-full h-full" title="Informations sur le séjour">
                 <div className="flex flex-row gap-2 items-center my-2 justify-between">
+                    <p>Nombre d'adultes:</p>
+                    <InputNumber value={infos.adultes} onValueChange={(e) => setNuit({...infos, adulte: e.value})} />
+                </div>
+                <div className="flex flex-row gap-2 items-center my-2 justify-between">
+                    <p>Nombre d'enfants:</p>
+                    <InputNumber value={infos.enfants} onValueChange={(e) => setNuit({...infos, enfants: e.value})} />
+                </div>
+                <h1 className="text-xl font-medium">Dates</h1>
+                <div className="flex flex-row gap-2 items-center my-2 justify-between">
                     <p>Jour d'arrivée:</p>
-                    <Calendar dateFormat="dd/mm/yy" showButtonBar  value={dates.debut} onChange={(e) => {if (e.value) setDates({...dates, debut: e.value}); else setDates({...dates, debut: undefined})}} />
+                    <Calendar dateFormat="dd/mm/yy" showButtonBar  value={infos.debut} onChange={(e) => {if (e.value) setInfos({...infos, debut: e.value}); else setInfos({...infos, debut: undefined})}} />
                 </div>
                 <div className="flex flex-row gap-2 items-center my-2 justify-between">
                     <p>Jour du départ:</p>
-                    <Calendar dateFormat="dd/mm/yy" showButtonBar  value={dates.fin} onChange={(e) => {if (e.value) setDates({...dates, fin: e.value}); else setDates({...dates, fin: undefined})}} />
+                    <Calendar dateFormat="dd/mm/yy" showButtonBar  value={infos.fin} onChange={(e) => {if (e.value) setInfos({...infos, fin: e.value}); else setInfos({...infos, fin: undefined})}} />
                 </div>
             </Card>
 
@@ -75,6 +84,7 @@ export default function DossierForm() {
                         {key: "dej", label: "Petit déjeuner", qte: 0, prixHt: 0, tva: "10", remise: 0},
                         {key: "enf", label: "Repas enfant", qte: 0, prixHt: 0, tva: "10", remise: 0},
                         {key: "coll", label: "Repas collègien", qte: 0, prixHt: 0, tva: "10", remise: 0},
+                        {key: "adlte", label: "Repas adulte", qte: 0, prixHt: 0, tva: "10", remise: 0},
                         {key: "piquenique", label: "Pique-nique indiv. enfant et adulte", qte: 0, prixHt: 0, tva: "10", remise: 0},
                         {key: "gouter", label: "Goûter enfant et adulte", qte: 0, prixHt: 0, tva: "5.5", remise: 0}
                     ]}
@@ -104,11 +114,15 @@ export default function DossierForm() {
                 />
             </Card>
 
+            <Card className="w-full h-full" title="Valier">
+                {/* btn de validation du form */}
+            </Card>
+
             <Debug>
                 <Card className="w-full h-full col-span-2 flex flex-col gap-2" title="INFOS">
                     <h1>
                         Dates:
-                        {JSON.stringify(dates)}
+                        {JSON.stringify(infos)}
                     </h1>
                     <h1>
                         Client:
