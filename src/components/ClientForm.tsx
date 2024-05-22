8430e20688b337781cb5f01a2f775326bbba7caf
BahAilime <emilia.beguin@gmail.com>
2024-05-22 14:56:19 +0200

    Fix css / media queries

--- src/components/ClientForm.tsx
+++ src/components/ClientForm.tsx
@@ -46,7 +46,7 @@ export default function ClientForm({ dataImport={}, onChange = (x: any) => {x} }
 
     return (
         <>
-            <div className="flex justify-center items-center w-full">
+            <div className="flex justify-center items-center w-full mt-3">
                 <SelectButton
                     value={data.type_cli}
                     onChange={(type) => addState("type_cli", type.value)}
@@ -55,14 +55,16 @@ export default function ClientForm({ dataImport={}, onChange = (x: any) => {x} }
                     options={optionsBtn}
                 />
             </div>
-            <FormInput value={data.nom_cli} label="Nom de la structure" name="name" onChange={(name) => addState("nom_cli", name)}/>
-            <FormInput value={data.resp_cli} label="Nom du responsable" name="resp" onChange={(resp) => addState("resp_cli", resp)}/>
-            <div className="flex w-full gap-4 flex-col lg:flex-row flex-wrap">
+            <div className="flex w-full gap-2 xl:flex-col 2xl:flex-row">
+                <FormInput value={data.nom_cli} label="Nom de la structure" name="name" onChange={(name) => addState("nom_cli", name)}/>
+                <FormInput value={data.resp_cli} label="Nom du responsable" name="resp" onChange={(resp) => addState("resp_cli", resp)}/>
+            </div>
+            <div className="flex w-full gap-2 xl:flex-col 2xl:flex-row">
                 <FormInput value={data.email_cli} label="Email" name="email" onChange={(email) => addState("email_cli", email)}  keyfilter="email"/>
                 <FormInput value={data.tel_cli} label="Téléphone" name="tel" onChange={(tel) => addState("tel_cli", tel)}/>
             </div>
             <FormInput value={data.adr_cli} label="Adresse" name="adr" onChange={(adr) => addState("adr_cli", adr)}/>
-            <div className="flex w-full gap-4 flex-wrap">
+            <div className="flex w-full gap-2 xl:flex-col 2xl:flex-row">
                 <FormInput value={data.ville_cli} label="Ville" name="ville" onChange={(ville) => addState("ville_cli", ville)}/>
                 <FormInput value={data.cp_cli} label="Code postal" name="cp" onChange={(cp) => addState("cp_cli", cp)} keyfilter="pnum"/>
             </div>
