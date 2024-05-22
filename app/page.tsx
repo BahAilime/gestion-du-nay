99b8b3185b163708dd996f2a9f0fefe3aeb113c6
BahAilime <emilia.beguin@gmail.com>
2024-05-22 09:05:12 +0200

    Test custom line

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import Loading from '@/src/components/Loading';
 import ReorderableParamsRow from '@/src/components/ReorderableParamsRow';
 import ReorderableParams from '@/src/components/ReorderableParams';
+import CustomLine from '@/src/components/CustomLine';
 
 export default function Home() {
   const [value, setValue] = useState(0);
@@ -21,10 +22,12 @@ export default function Home() {
         {/* <ReorderableParamsRow label="Enfants" unit="ans" minVal={4} maxVal={7} qteBase={14} onChange={(x) => console.log(x)}/> */}
       {/* </div> */}
       <ReorderableParams rows={[
-        {key:"1", label: "Enfants", minAge: 0, maxAge: 12, qte: 22},
-        {key:"2", label: "Ado", minAge: 13, maxAge: 17, qte: 25},
-        {key:"3", label: "Adulte", minAge: 18, maxAge: 99, qte: 7}
+        {key:"sdrkuvgnsj", label: "Enfants", qte: 22},
+        {key:"vseffiubgv", label: "Ado", qte: 25},
+        {key:"ehjbvsjhtv", label: "Adulte", qte: 7},
+        {key:"klkjbhvgvs", label: "VIEUX", qte: 7}
         ]} onChange={(x) => console.log(x)}/>
+      <CustomLine id='1' label='Hejjj' prixHt={12} tvaBase='20' remiseBase={10} qteBase={5} />
     </>
   );
 }
