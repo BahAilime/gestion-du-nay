b038e66f7288de40b56af7186f9c35ecc782f8a9
BahAilime <emilia.beguin@gmail.com>
2024-05-23 15:24:56 +0200

    Ajout des types de client, line et dossier

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,10 +1,46 @@
 import { db } from "@/src/services/firebase";
-import { ref, child, get, onValue, push, update, remove, serverTimestamp, query, orderByChild} from "firebase/database";
+import { ref, child, get, onValue, push, update, remove, serverTimestamp, query, orderByChild, DatabaseReference} from "firebase/database";
 
+export type client = {
+    nom_cli: string;
+    resp_cli?: string;
+    email_cli?: string;
+    tel_cli?: string;
+    adresse_cli?: string;
+    ville_cli?: string;
+    cp_cli?: string;
+    type_cli?: string;
+    notes_cli?: string;
+    lastSeen?: any;
+}
+
+export type line = {
+    key?: string,
+    label?: string,
+    qte?: number,
+    prixHt?: number,
+    tva?: number,
+    remise?: number
+}
+
+export type dossier = {
+    idClient: string,
+    nuits?: {
+        drap?: number,
+        lines?: line[]},
+    repas?: {
+        servi?: boolean,
+        lines?: line[]
+    },
+    activite?: line[],
+    divers?: line[],
+    lastSeen?: any;
+}
 
 const clientsRef = ref(db, "client");
+const dossierRef = ref(db, "dossier");
 
-export function getClientOnce(id: number|string): Promise<any> {
+export function getClientOnce(id: number|string): Promise<client> {
     return get(child(clientsRef, `${id}`))
         .then((snapshot) => {
             if (snapshot.exists()) {
@@ -18,7 +54,7 @@ export function getClientOnce(id: number|string): Promise<any> {
         });
 }
 
-export function getClientsOnce(): Promise<any[]> {
+export function getClientsOnce(): Promise<client[]> {
     return get(query(clientsRef, orderByChild("lastSeen")))
         .then((snapshot) => {
             if (snapshot.exists()) {
@@ -36,19 +72,20 @@ export function getClientsOnce(): Promise<any[]> {
         });
 }
 
-export function subscribeToClient(id: number, callback: (client: any) => void) {
+export function subscribeToClient(id: number|string, callback: (client: client) => void) {
     onValue(child(clientsRef, `${id}`), (snapshot) => {
         callback(snapshot.val());
       });
 }
 
-export function subscribeToClients(callback: (clients: any) => void) {
+export function subscribeToClients(callback: (clients: client[]) => void) {
     onValue(clientsRef, (snapshot) => {
         callback(snapshot.val());
       });
 }
 
-export function newClient(data: any, callback: (client: any) => void) {
+// TODO: Make it return a client object
+export function newClient(data: client, callback: (client: DatabaseReference) => void) {
     data.lastSeen = serverTimestamp()
     push(clientsRef, data)
         .then((newCli) => {
@@ -56,7 +93,7 @@ export function newClient(data: any, callback: (client: any) => void) {
         })
 }
 
-export function updateClient(id: number|string, data: any, callback: () => void) {
+export function updateClient(id: number|string, data: client|any, callback: () => void) {
     data.lastSeen = serverTimestamp()
     get(child(clientsRef, `${id}`))
         .then((snapshot) => {
