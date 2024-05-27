981eaf3ad73f02148af60431411d8b70f4aaa3cd
BahAilime <emilia.beguin@gmail.com>
2024-05-27 08:54:07 +0200

    Ajout de la page de la liste des dossiers

--- app/dossier/page.tsx
+++ app/dossier/page.tsx
@@ -5,7 +5,8 @@ import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { useRouter } from 'next/navigation'
 import Loading from '@/src/components/Loading';
-import { getClientsOnce, getDossiersOnce } from '@/src/services/db';
+import { dossier, getClientsOnce, getDossiersOnce } from '@/src/services/db';
+import { format } from 'date-fns';
 
 export default function Home() {
     const [dossiers, setDossiers] = useState<any[]>();
@@ -13,21 +14,31 @@ export default function Home() {
 
     useEffect(() => {
         getDossiersOnce().then((data) => {
+            console.log(data);
             setDossiers(data);
         });
     }, [])
 
+    function dateTemplate(data: dossier) {
+        if (!data?.infos?.debut || !data?.infos?.fin) {
+            return <div>Pas encore de dates</div>
+        }
+        const deb = format(data.infos.debut, "dd/MM/yyyy")
+        const fin = format(data.infos.fin, "dd/MM/yyyy")
+        const nuits = data.infos.nuits
+
+        return <div>Du <span className="font-bold">{deb}</span> au <span className="font-bold">{fin}</span> ({nuits} nuits)</div>
+    }
+
     return (
         <Card title="Dossiers" className="w-full h-full">
-            {JSON.stringify(dossiers)}
-            {/* {client ?
-            <DataTable  value={client} className="w-full" selectionMode="single" onSelectionChange={(e) => router.push(`/client/detail?firebase_id=${e.value.firebase_id}`)}>
-                <Column field="nom_cli" header="Nom"></Column>
-                <Column field="resp_cli" header="Resp"></Column>
-                <Column field="email_cli" header="Email"></Column>
-                <Column field="tel_cli" header="Tel"></Column>
+            {dossiers ?
+            <DataTable  value={dossiers} className="w-full" selectionMode="single" onSelectionChange={(e) => router.push(`/dossier/detail?firebase_id=${e.value.firebase_id}`)}>
+                <Column body={(data: dossier) => <div><span className="font-bold">{data?.client?.nom_cli}</span><br/>{data?.client?.resp_cli}</div>} header="Client"></Column>
+                <Column body={dateTemplate} header="Dates"></Column>
+                <Column header="Effectif" body={(data: dossier) => <div>{data?.infos?.enfants} enfants et {data?.infos?.adultes} adultes</div>}></Column>
             </DataTable>
-            : <Loading text={"Chargement des clients..."}/>} */}
+            : <Loading text={"Chargement des dossiers..."}/>}
         </Card>
     );
   }
