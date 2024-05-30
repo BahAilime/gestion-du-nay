bf9346a9160fa1de84bd82244642659c0f2b0f8b
BahAilime <emilia.beguin@gmail.com>
2024-05-30 14:06:27 +0200

    Suppr des tests de la page d'accueil pour que ca soit présentable

--- app/page.tsx
+++ app/page.tsx
@@ -14,20 +14,7 @@ export default function Home() {
   const [value, setValue] = useState(0);
   return (
     <>
-      <h1>Accueil</h1>
-      <Knob value={value} onChange={(e) => setValue(e.value)} />
-      <FontAwesomeIcon icon={faEnvelope} />
-      <Loading />
-      {/* <div className='max-w-96'> */}
-        {/* <ReorderableParamsRow label="Enfants" unit="ans" minVal={4} maxVal={7} qteBase={14} onChange={(x) => console.log(x)}/> */}
-      {/* </div> */}
-      <ReorderableParams rows={[
-        {key:"sdrkuvgnsj", label: "Enfants", qte: 22},
-        {key:"vseffiubgv", label: "Ado", qte: 25},
-        {key:"ehjbvsjhtv", label: "Adulte", qte: 7},
-        {key:"klkjbhvgvs", label: "VIEUX", qte: 7}
-        ]} onChange={(x) => console.log(x)}/>
-      <CustomLine id='1' label='Hejjj' prixHt={12} tvaBase={20} remiseBase={10} qteBase={5} />
+      <h1>La page d'accueuil et les statistiques arrivent bientôt ! :D</h1>
     </>
   );
 }
