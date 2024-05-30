88bb67048131ccd0c64602112fb14cbc95928c31
BahAilime <emilia.beguin@gmail.com>
2024-05-30 14:44:47 +0200

    Fix &apos;

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1>La page d'accueuil et les statistiques arrivent bientôt ! :D</h1>
+      <h1>La page d&apos;accueuil et les statistiques arrivent bientôt ! :D</h1>
     </>
   );
 }
